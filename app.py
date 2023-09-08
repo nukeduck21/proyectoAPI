019709d442f4e90baf868afc3c12a3fbac39f1eb
oscar <lucio3143187070@gmail.com>
2023-09-08 05:07:56 -0500

    Merge remote-tracking branch 'origin/main'

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
 import firebase_admin
-from firebase_admin import credentials, firestore, auth
+from firebase_admin import credentials, firestore
 
-app = Flask(__name__)
-app.secret_key = 'acm1pte'  # Cambia esto por una clave secreta
-
-# Configura Firebase Admin SDK
-cred = credentials.Certificate("api-imc-firebase-adminsdk-f3xab-4321a2ccf0.json")
+# Inicializa Firebase App
+cred = credentials.Certificate("ruta/a/tu/credencial-de-servicio.json")
 firebase_admin.initialize_app(cred)
 
-# Inicializa Firestore
+# Configura el cliente de Firestore
 db = firestore.client()
 
 # Ruta principal
@@ -17,10 +13,9 @@ db = firestore.client()
 def index():
     if request.method == 'POST':
         email = request.form['email']
-        password = request.form['password']  # Obtén la contraseña del formulario
+        imc = float(request.form['imc'])
 
         # Determina el mensaje según el IMC
-        imc = float(request.form['imc'])
         if imc < 18.5:
             message = 'Estás delgado.'
         elif imc >= 18.5 and imc < 25:
@@ -28,27 +23,20 @@ def index():
         else:
             message = 'Estás gordo.'
 
-        try:
-            # Crea un nuevo usuario en Firebase Authentication con la contraseña proporcionada
-            user = auth.create_user(
-                email=email,
-                password=password
-            )
+        # Guarda los datos en Firestore
+        data = {
+            'email': email,
+            'imc': imc,
+            'message': message
+        }
+        db.collection('usuarios').add(data)
 
-            # Almacena el mensaje en Firestore
-            user_ref = db.collection('users').document(user.uid)
-            user_ref.set({
-                'email': email,
-                'imc': imc,
-                'message': message
-            })
-
-            flash('Correo enviado con éxito', 'success')
-        except Exception as e:
-            flash('Error al crear el usuario: ' + str(e), 'error')
+        # Envía una respuesta al usuario
+        return f'¡Tus datos se han guardado en Firestore! Tu IMC es {imc}.'
 
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
 
